def13e310dafe9d07848525e62ce485fa37a44a5
Jonatandb <jonatandb@gmail.com>
2022-05-12 23:57:32 -0300

    1.4: Información del curso, paso 4
    https://fullstackopen.com/es/part1/java_script#ejercicios-1-3-1-5

--- part1/react-introduction/src/index.js
+++ part1/react-introduction/src/index.js
@@ -17,30 +17,35 @@ const Content = ({ parts: [part1, part2, part3] }) => (
   </>
 )
 
-const Footer = ({ exercises: [exercises1, exercises2, exercises3] }) => (
-  <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+const Total = ({ parts }) => (
+  <p>
+    Number of exercises{' '}
+    {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+  </p>
 )
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Footer exercises={[part1.exercises, part2.exercises, part2.exercises]} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
