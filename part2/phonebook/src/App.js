0acc0d013ab27b367a7bab724eeb1ac2654a1166
Jonatandb <jonatandb@gmail.com>
2022-05-19 00:03:51 -0300

    2.8: La guía telefónica, paso 3
    https://fullstackopen.com/es/part2/formularios#ejercicios-2-6-2-10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,30 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phone: '040-123456' },
+  ])
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
+  const [filter, setFilter] = useState('')
+
+  const results =
+    filter === ''
+      ? persons
+      : persons.filter(person =>
+          person.name.toLowerCase().includes(filter.toLowerCase()),
+        )
 
   const handleNameChanged = e => {
     setNewName(e.target.value)
+  }
+
+  const handlePhoneChanged = e => {
+    setNewPhone(e.target.value)
+  }
+
+  const handleFilterChanged = e => {
+    setFilter(e.target.value)
   }
 
   const handleSumbit = e => {
@@ -13,25 +32,36 @@ const App = () => {
     if (persons.find(p => p.name === newName)) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      setPersons([...persons, { name: newName }])
+      setPersons([...persons, { name: newName, phone: newPhone }])
       setNewName('')
+      setNewPhone('')
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with:{' '}
+        <input onChange={handleFilterChanged} value={filter} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={handleSumbit}>
         <div>
           Name: <input onChange={handleNameChanged} value={newName} />
+        </div>
+        <div>
+          Number: <input onChange={handlePhoneChanged} value={newPhone} />
         </div>
         <div>
           <button type='submit'>Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => (
-        <p key={person.name}>{person.name}</p>
+      {results.map(person => (
+        <p key={person.name}>
+          {person.name} {person.phone}
+        </p>
       ))}
     </div>
   )
