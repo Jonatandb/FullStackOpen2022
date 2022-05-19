899c99393252aef02b57509ef31535efea232833
Jonatandb <jonatandb@gmail.com>
2022-05-18 23:52:49 -0300

    2.6: La guía telefónica, paso 1
    https://fullstackopen.com/es/part2/formularios#ejercicios-2-6-2-10

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+
+  const handleNameChanged = e => {
+    setNewName(e.target.value)
+  }
+
+  const handleSumbit = e => {
+    e.preventDefault()
+    setPersons([...persons, { name: newName }])
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSumbit}>
+        <div>
+          Name: <input onChange={handleNameChanged} value={newName} />
+        </div>
+        <div>
+          <button type='submit'>Add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  )
+}
+
+export default App
